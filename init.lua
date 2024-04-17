2debae3c8271569e3bc582cbc64998c7dae0a3f0
Priyam More <morepriyam99@gmail.com>
2024-04-17 23:44:35 +0530

    filestructure

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>v", vim.cmd.Ex)
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -14,27 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-{
-   'nvim-telescope/telescope.nvim', tag = '0.1.6',
-   -- or                              , branch = '0.1.x',
-   dependencies = { 'nvim-lua/plenary.nvim' }
-}
-}
 
-local opts = {}
+require("options")
+require("lazy").setup("plugins")
 
-require("lazy").setup(plugins, opts)
-
---Telescope
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
-
--- Colours
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
